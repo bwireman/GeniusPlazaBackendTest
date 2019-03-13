50ae440b5288f9544365ee1ce2b9c23a37f6d35a
bwireman <bwireman@umich.edu>
2019-03-13 10:47:31 -0400

    tests and models

--- backendtest/api/models.py
+++ backendtest/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+class Step(models.Model):
+    step_text = models.CharField(max_length=100)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+class Ingredient(models.Model):
+    text = models.CharField(max_length=100)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
--- backendtest/api/tests.py
+++ backendtest/api/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
-
+from .models import Recipe, Ingredient, Step
+from django.contrib.auth.models import User
 # Create your tests here.
+class ModelTestCase(TestCase):
+
+    def test_create_recipe_with_ingredients_and_steps(self):
+
+        # gerneric user
+        self.user = User()
+        self.user.save()
+        
+        # test recipe
+        self.recipe = Recipe(name="test recipe", user=self.user)
+
+        old_count = Recipe.objects.count()
+        self.recipe.save()
+        new_count = Recipe.objects.count()
+        self.assertNotEqual(old_count, new_count)
+
+        # test ingredient
+        self.ingredient = Ingredient(text="test ingredient", recipe=self.recipe)
+
+        old_count = Ingredient.objects.count()
+        self.ingredient.save()
+        new_count = Ingredient.objects.count()
+        self.assertNotEqual(old_count, new_count)
+
+        # test step
+        self.step = Step(step_text="test step", recipe=self.recipe)
+
+        old_count = Step.objects.count()
+        self.step.save()
+        new_count = Step.objects.count()
+        self.assertNotEqual(old_count, new_count)
